ad6f0113959cfac3b8534f1ebc17f3e67a4c0eb6
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-07-06 11:50:36 +0100

    Finalized fetch API and DOM manipulation

--- js/app.js
+++ js/app.js
@@ -34,10 +34,6 @@ let searchMovie = async(id, callback) => {
     return moviedb_find_response;
 }
 
-let checkCategory = async(query, callback) => {
-    
-}
-
 let getUpcomingMovies = async(query, callback) => {
     let data = await checkSearch("COMING_SOON").then(
         data => {
@@ -46,6 +42,7 @@ let getUpcomingMovies = async(query, callback) => {
                     data.results.splice(i, i+1);
                 }
             }
+            return data;
         }
     );
     return data; 
@@ -58,7 +55,7 @@ let getRecentMovies = async() => {
 
 let getCategory = async () => {
     
-
+    // Hehe
 
 }
 
@@ -74,27 +71,33 @@ let getRandomMovie = async() => {
     let dataContainer = [];
     for(let i = 0; i < 20; i++){
         dataContainer[i] = await searchMovie(randomIDarr[i]);
-        if(dataContainer[i].adult == false)
+        if(dataContainer[i].adult == true) // No adult films :)
         {
+            dataContainer.splice(i, i+1);
             break;
+        } else if(dataContainer[i].hasOwnProperty("success") ){
+            dataContainer.splice(i, i+1)
         }
     }
+    data = dataContainer;
     return data;
 }
 
 let manipulateMovieOfTheDay = async(callback) => {
-    let movieData = await getRandomMovie();
+    let movieDataArr = await getRandomMovie();
+    let movieData = movieDataArr[0];
     let movieTitle = document.getElementById("pick_day_title");
     let description = document.getElementById("pick_day_desc");
     let poster = document.getElementById("pick_day_poster");
     let redirect = document.getElementById("pick_mdb_red");
+
     if(movieData.poster_path == null){
         poster.src = "https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg";
     } else {
-        poster.src = `https://image.tmdb.org/t/p/w185${movieData.poster_path}`;
+        poster.src = `https://image.tmdb.org/t/p/w500/${movieData.poster_path}`;
     }
 
-    if(movieData.overview == null){
+    if(movieData.overview.length == 0){
         description.textContent = "There is no description provided.";
     } else {
         description.textContent = movieData.overview;
@@ -102,19 +105,61 @@ let manipulateMovieOfTheDay = async(callback) => {
 
     movieTitle.textContent = movieData.original_title;
     redirect.href = `https://www.themoviedb.org/movie/${movieData.id}`;
-
-
-    console.log(movieData);
-
-    title.textContent = movieData.overview;
 }
 
 let manipulateLatestMovies = async(callback) => {
-
+    let data = await getRecentMovies().then(result => {return result.results}).then(
+        data => {
+            for(let i = 0; i < 4; i++){
+                let title = document.getElementById(`lat_${i}_title`);
+                let poster = document.getElementById(`lat_${i}_post`);
+                let description = document.getElementById(`lat_${i}_desc`);
+                let redirect = document.getElementById(`lat_${i}_red`);
+        
+                if(data[i].poster_path == null){
+                    poster.src = "https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg";
+                } else {
+                    poster.src = `https://image.tmdb.org/t/p/w500/${data[i].poster_path}`;
+                }
+            
+                if(data[i].overview.length == 0){
+                    description.textContent = "There is no description provided.";
+                } else {
+                    description.textContent = data[i].overview;
+                }
+                redirect.href = `https://www.themoviedb.org/movie/${data[i].id}`;
+                title.textContent = data[i].original_title;
+            }
+        }
+    );
 }
 
 let manipulateComingSoon = async(callback) => {
+    let data = await getUpcomingMovies().then(result => {return result.results});
+    console.log(data);
+    for(let i = 0; i < 4; i++){
+        let title = document.getElementById(`com_${i}_title`);
+        let poster = document.getElementById(`com_${i}_post`);
+        let description = document.getElementById(`com_${i}_desc`);
+        let redirect = document.getElementById(`com_${i}_red`);
 
+        if(data[i].poster_path == null){
+            poster.src = "https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg";
+        } else {
+            poster.src = `https://image.tmdb.org/t/p/w500/${data[i].poster_path}`;
+        }
+    
+        if(data[i].overview.length == 0){
+            description.textContent = "There is no description provided.";
+        } else {
+            description.textContent = data[i].overview;
+        }
+
+        redirect.href = `https://www.themoviedb.org/movie/${data[i].id}`;
+        title.textContent = data[i].original_title;
+    }
 }
 
 manipulateMovieOfTheDay();
+manipulateLatestMovies();
+manipulateComingSoon();
