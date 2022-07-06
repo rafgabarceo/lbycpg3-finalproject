198cc906873e3833e84a0e127d0eb3a3be54d68b
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-07-06 09:20:54 +0100

    Added necessary functions for API calls. All return promise

--- js/app.js
+++ js/app.js
@@ -21,11 +21,34 @@ let checkSearch = async(query,callback) => {
     } else if(query == "UPCOMING_MOVIES"){
         let moviedb_recent_response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${_apikey}`).then(response => response.json());
         return moviedb_recent_response;
+    } else if(query == "COMING_SOON"){
+        let moviedb_recent_response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${_apikey}&language=en-US&page=1`).then(response => response.json());
+        return moviedb_recent_response; 
     }
+}
+
+let searchMovie = async(id, callback) => {
+    let _apikey = await getAPIKey();
+    _apikey =  _apikey["api-key"];
+    let moviedb_find_response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${_apikey}`).then(response => response.json());
+    return moviedb_find_response;
 }
 
 let checkCategory = async(query, callback) => {
     
+}
+
+let getUpcomingMovies = async(query, callback) => {
+    let data = await checkSearch("COMING_SOON").then(
+        data => {
+            for(let i = 0; i < data.results.length; i++){
+                if(data.results[i].adult == true){
+                    data.results.splice(i, i+1);
+                }
+            }
+        }
+    );
+    return data; 
 }
 
 let getRecentMovies = async() => {
@@ -42,17 +65,31 @@ let getCategory = async () => {
 let getRandomMovie = async() => {
     let data = await checkSearch("RANDOM_MOVIE")
         .then(response => {return response});
+    let latestId = data.id;
+    let randomIDarr = [];
+    for(let i = 0; i < 20; i++){
+        randomIDarr[i] = Math.floor(Math.random() * latestId);
+    }
+
+    let dataContainer = [];
+    for(let i = 0; i < 20; i++){
+        dataContainer[i] = await searchMovie(randomIDarr[i]);
+        if(dataContainer[i].adult == false)
+        {
+            break;
+        }
+    }
     return data;
 }
 
-let manipulatePosters = async () => {
-    let data = await getRecentMovies(); // Return a promise. Because we attached await, will attach value.
-    let pos1 = document.getElementById("pos1");
-    console.log(data);
-    let imageData = data.results[0].poster_path;
-    let posterPath = `https://image.tmdb.org/t/p/w185${imageData}`;
-    pos1.src = posterPath;
+let manipulateMovieOfTheDay = async(query, callback) => {
 
 }
 
-manipulatePosters();
+let manipulateLatestMovies = async(query, callback) => {
+
+}
+
+let manipulateComingSoon = async(query, callback) => {
+
+}
